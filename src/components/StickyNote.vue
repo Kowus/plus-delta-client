bda8c446b5ce6bbcfc75eaa901989e3cadecc0ea
Kowus <barnabas@elite-education.org>
2019-09-13 23:39:50 +0000

    handle edit modes

--- src/components/StickyNote.vue
+++ src/components/StickyNote.vue
@@ -1,26 +1,78 @@
 <template>
   <div class="card note" :class="`border-${note.class === 'plus'?'warning':'danger'}`">
     <div class="card-body">
-      <h5 class="card-title">
-        {{note.title}}
-        <small class="note-editor">
-          <i class="fas fa-edit" @click="setCurEdit(note._id)"></i>
-        </small>
-      </h5>
+      <small class="note-editor">
+        <i class="fas fa-edit" @click="setCurEdit(note._id)" v-if="getCurEdit !== note._id"></i>
+        <button type="button" class="btn btn-danger btn-sm" v-if="getCurEdit === note._id">
+          <i class="fas fa-trash-alt"></i>
+        </button>
+        <button
+          type="button"
+          class="btn btn-success btn-sm"
+          v-if="getCurEdit === note._id"
+          @click="sBlur({id:note._id})"
+        >
+          <i class="fas fa-save"></i>
+        </button>
+      </small>
+      <div class="form-group" v-if="getCurEdit === note._id">
+        <input
+          class="form-control"
+          id="projectTitle"
+          aria-describedby="titleHelp"
+          placeholder="Review Title"
+          v-model="note.title"
+        />
+        <small id="titleHelp" class="form-text text-muted">Enter title for review</small>
+      </div>
+      <h5 class="card-title" v-else>{{note.title}}</h5>
       <img src="/img/tack.png" class="tack" />
-      <p
-        class="card-text"
-        :contenteditable="getCurEdit === note._id"
-        @blur="sBlur({id:note._id})"
-      >{{note.message}}</p>
+      <textarea
+        class="form-control"
+        id="exampleTextarea"
+        rows="5"
+        style="margin-top: 0px; margin-bottom: 0px;"
+        v-model="note.message"
+        v-if="getCurEdit === note._id"
+      ></textarea>
+      <p class="card-text" v-else>{{note.message}}</p>
+
       <div class="preloader" v-if="note.blurred">
         <div class="spinner">
           <i class="fas fa-spinner"></i>
         </div>
       </div>
       <p class="text-right">
-        <small>-- {{revDate(note.date)}}</small>
+        <small class="text-muted">-- {{revDate(note.date)}}</small>
       </p>
+      <fieldset class="form-group fgs">
+        <div class="form-check" v-if="getCurEdit === note._id">
+          <label class="form-check-label text-warning">
+            <input
+              type="radio"
+              class="form-check-input"
+              name="optionsRadios"
+              id="optionsRadios1"
+              value="plus"
+              v-model="note.class"
+              checked
+            />
+            Plus
+          </label>
+          <span class="plus-delta-separator">|</span>
+          <label class="form-check-label text-danger">
+            <input
+              type="radio"
+              class="form-check-input"
+              name="optionsRadios"
+              id="optionsRadios2"
+              value="delta"
+              v-model="note.class"
+            />
+            Delta
+          </label>
+        </div>
+      </fieldset>
     </div>
   </div>
 </template>
@@ -49,6 +101,7 @@ export default {
       var scope = this;
       setTimeout(function() {
         scope.note.blurred = false;
+        if (scope.getCurEdit === scope.note._id) return scope.setCurEdit("");
       }, 5000);
     }
   }
@@ -56,6 +109,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+[contenteditable="true"] {
+  border: 1px solid black;
+  padding: 3px;
+  outline: none;
+}
+.btn {
+  padding: 2px;
+}
+
+#projectTitle {
+  // background-image: url("&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABHklEQVQ4EaVTO26DQBD1ohQWaS2lg9JybZ+AK7hNwx2oIoVf4UPQ0Lj1FdKktevIpel8AKNUkDcWMxpgSaIEaTVv3sx7uztiTdu2s/98DywOw3Dued4Who/M2aIx5lZV1aEsy0+qiwHELyi+Ytl0PQ69SxAxkWIA4RMRTdNsKE59juMcuZd6xIAFeZ6fGCdJ8kY4y7KAuTRNGd7jyEBXsdOPE3a0QGPsniOnnYMO67LgSQN9T41F2QGrQRRFCwyzoIF2qyBuKKbcOgPXdVeY9rMWgNsjf9ccYesJhk3f5dYT1HX9gR0LLQR30TnjkUEcx2uIuS4RnI+aj6sJR0AM8AaumPaM/rRehyWhXqbFAA9kh3/8/NvHxAYGAsZ/il8IalkCLBfNVAAAAABJRU5ErkJggg==&quot;");
+  background-repeat: no-repeat;
+  background-attachment: scroll;
+  background-size: 16px 18px;
+  background-position: 98% 50%;
+  cursor: auto;
+}
 .note {
   margin: 5px;
   text-align: left;
@@ -81,7 +151,9 @@ export default {
     }
   }
 }
-
+.fgs {
+  display: inline;
+}
 @keyframes spinner {
   0% {
     transform: rotate(0deg);
@@ -115,5 +187,8 @@ export default {
   animation-timing-function: linear;
   animation-name: spinner;
   animation-duration: 1s;
+}
+.plus-delta-separator {
+  margin: auto 5px;
 }
 </style>
